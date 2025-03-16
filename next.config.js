ad12804200563339e1485eef3b6e870dfa7f3392
Peter Kibuchi <68213317+peterkibuchi@users.noreply.github.com>
2025-03-16 14:31:42 +0300

    chore: update Next.js config to ignore linting and typechecking during builds

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
+ * This is especially useful for Docker builds.
  */
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  /** We already do linting and typechecking as separate tasks in CI */
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+};
 
 export default config;
