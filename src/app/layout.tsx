d18efb56fdb86666d8f78792d011dfdbd4101286
Glen Ochieng <glenochieng045@gmail.com>
2025-03-22 23:02:34 +0300

    fix: removed unused imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,6 @@
 import { type Metadata } from "next";
 import { Geist } from "next/font/google";
-import {
-  ClerkProvider,
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  SignUpButton,
-  UserButton,
-} from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 
 import "~/styles/globals.css";
 
