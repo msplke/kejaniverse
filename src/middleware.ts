ed604a6d3e6bdfdeb1d30dba0ee3d8df0925d225
Glen Ochieng <glenochieng045@gmail.com>
2025-04-06 07:09:32 +0300

    fix: add properties route to protected routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const protectedRoutes = createRouteMatcher(["/dashboard(.*)"]);
+const protectedRoutes = createRouteMatcher(["/properties(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
   if (protectedRoutes(req)) await auth.protect();
