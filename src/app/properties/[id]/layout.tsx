0a41d1137c7ab0d4c28b51f2878ae1222637f674
Glen Ochieng <glenochieng045@gmail.com>
2025-04-10 20:27:16 +0300

    refactor: move app-sidebar to components folder

--- src/app/properties/[id]/layout.tsx
+++ src/app/properties/[id]/layout.tsx
@@ -1,4 +1,4 @@
-import { AppSidebar } from "~/app/_components/app-sidebar";
+import { AppSidebar } from "~/components/ui/app-sidebar";
 import { SidebarProvider, SidebarTrigger } from "~/components/ui/sidebar";
 
 type Params = Promise<{ id: string }>;
