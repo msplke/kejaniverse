c4edfd51d5b3a9598e289de9b743e65f72b0b0c9
Glen Ochieng <glenochieng045@gmail.com>
2025-04-20 00:21:11 +0300

    refactor: move a few validators to their own file

--- src/app/api/callbacks/ussd/route.ts
+++ src/app/api/callbacks/ussd/route.ts
@@ -1,33 +1,20 @@
-import { z } from "zod";
-import { zfd } from "zod-form-data";
-
 import { env } from "~/env";
-import { getTenantByUnitName, getUnitbyName } from "~/server/actions";
-
-const schema = zfd.formData({
-  sessionId: zfd.text(),
-  serviceCode: zfd.text(),
-  phoneNumber: zfd.text(),
-  text: z.string(),
-});
-
-const chargeApiRequestSchema = z.object({
-  amount: z.number().positive(),
-  email: z.string().email(),
-  mobile_money: z.object({
-    phone: z.string().regex(/\+254\d{9}/),
-    provider: z.literal("mpesa"),
-  }),
-});
-
-type ChargeApiRequest = z.infer<typeof chargeApiRequestSchema>;
+import { getTenantByUnitName } from "~/server/actions";
+import {
+  formDataSchema,
+  validateAmount,
+  validateChargeApiRequestData,
+  validatePhoneNumber,
+  validateUnitName,
+  type ChargeApiRequest,
+} from "./input-validators";
 
 const responseHeaders = {
   "Content-Type": "application/text",
 };
 
 export async function POST(req: Request) {
-  const validation = schema.safeParse(await req.formData());
+  const validation = formDataSchema.safeParse(await req.formData());
   if (!validation.success) {
     console.error("Invalid form parameters", validation.error.format());
     return new Response("END Something went wrong. Please try again later.", {
@@ -46,53 +33,37 @@ export async function POST(req: Request) {
   }
 
   const prevResponses = text.split("*");
-
-  console.log(`text=${text}`);
-  console.log(`responses=${prevResponses.length}`);
-
-  let response;
+  let responseText;
 
   if (prevResponses.length === 1) {
-    // Validate that the unit exists
-    const unitName = prevResponses[0]!;
-    try {
-      await getUnitbyName(unitName);
-    } catch (error) {
-      console.error(error);
-      return new Response("END Unit not found. Please try again.", {
+    const validationResult = await validateUnitName(prevResponses[0]!);
+    if (validationResult.status === "invalid") {
+      return new Response(validationResult.message, {
         headers: responseHeaders,
       });
     }
-
-    // The unit exists, ask for the amount to pay
-    response = "CON Enter the amount you want to pay.";
+    responseText = "CON Enter the amount you want to pay.";
   } else if (prevResponses.length === 2) {
-    // Validate that the amount is a number
-    const amount = prevResponses[1]!;
-    const validation = z.coerce.number().positive().safeParse(amount);
-    if (!validation.success) {
-      return new Response("END Invalid amount. Please try again.", {
+    const validationResult = validateAmount(prevResponses[1]!);
+    if (validationResult.status === "invalid") {
+      return new Response(validationResult.message, {
         headers: responseHeaders,
       });
     }
 
-    // The amount is valid, ask for the amount to be paid
-    response =
+    responseText =
       "CON Enter the M-Pesa number you want to pay with e.g. +254712345678";
   } else if (prevResponses.length === 3) {
-    // Validate that the phone number is in the correct format
-    const phoneNumber = prevResponses[2]!;
-    const phoneNumberRegex = /^\+254\d{9}$/;
-    if (!phoneNumberRegex.test(phoneNumber)) {
-      return new Response("END Invalid phone number. Please try again.", {
+    const validationResult = validatePhoneNumber(prevResponses[2]!);
+    if (validationResult.status === "invalid") {
+      return new Response(validationResult.message, {
         headers: responseHeaders,
       });
     }
-
+    // The phone number is valid, ask the user to confirm payment
     const amount = prevResponses[1];
     const unitName = prevResponses[0];
-    // The phone number is valid, ask the user to confirm payment
-    response = `CON Do you want to pay KES ${amount} for unit ${unitName}?\n1. Yes\n2. No`;
+    responseText = `CON Do you want to pay KES ${amount} for unit ${unitName}?\n1. Yes\n2. No`;
   } else if (prevResponses.length === 4) {
     // Validate the user's choice
     if (prevResponses[3] === "1") {
@@ -111,6 +82,7 @@ export async function POST(req: Request) {
           headers: responseHeaders,
         });
       }
+
       const data: ChargeApiRequest = {
         // Paystack parses the amount in subunits, so we multiply by 100
         // after parsing it to an integer
@@ -122,16 +94,15 @@ export async function POST(req: Request) {
         },
       };
 
-      const validation = chargeApiRequestSchema.safeParse(data);
-      if (!validation.success) {
-        console.error("Invalid charge API request", validation.error.format());
-        return new Response(
-          "END Something went wrong. Please try again later.",
-          { headers: responseHeaders },
-        );
+      const validation = validateChargeApiRequestData(data);
+      if (validation.status === "invalid") {
+        return new Response(validation.message, {
+          headers: responseHeaders,
+        });
       }
 
-      console.log(validation.data);
+      // The charge request is valid, proceed with the payment
+      console.log(data);
 
       try {
         const paystackResponse = await fetch("https://api.paystack.co/charge", {
@@ -140,25 +111,25 @@ export async function POST(req: Request) {
             Authorization: `Bearer ${env.PAYSTACK_LIVE_SECRET_KEY}`,
             "Content-Type": "application/json",
           },
-          body: JSON.stringify(validation.data),
+          body: JSON.stringify(data),
         });
 
         if (paystackResponse.status !== 200) {
           console.log(await paystackResponse.json());
-          response = "END Transaction failed. Please try again later.";
+          responseText = "END Transaction failed. Please try again later.";
         } else {
-          response = "END You'll receive an M-Pesa prompt shortly.";
+          responseText = "END You'll receive an M-Pesa prompt shortly.";
         }
       } catch (error) {
         console.error(error);
-        response = "END Transaction failed. Please try again later.";
+        responseText = "END Transaction failed. Please try again later.";
       }
     } else {
-      response = "END Transaction cancelled.";
+      responseText = "END Transaction cancelled.";
     }
   }
 
-  return new Response(response, {
+  return new Response(responseText, {
     headers: responseHeaders,
   });
 }
