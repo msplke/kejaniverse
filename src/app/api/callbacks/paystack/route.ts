68287baaa85e17ad8957a0ae0205a68ee5161363
Glen Ochieng <glenochieng045@gmail.com>
2025-04-21 10:42:15 +0300

    fix: address some eslint warnings and errors

--- src/app/api/callbacks/paystack/route.ts
+++ src/app/api/callbacks/paystack/route.ts
@@ -1,11 +1,6 @@
 export async function POST(req: Request) {
-  const body = await req.json();
-  console.log("Body:");
-  console.log(body);
-  const event = body.event;
-
   console.log("[Paystack Callback]");
-  console.log(event);
+  console.log(await req.json());
   console.log("[Paystack Callback]");
 
   return new Response("OK", { status: 200 });
