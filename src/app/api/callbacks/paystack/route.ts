08056298390da7501ab0c79d56bcc23a1cc8612f
Glen Ochieng <glenochieng045@gmail.com>
2025-04-21 17:04:17 +0300

    refactor: remove currently unused callback route

--- src/app/api/callbacks/paystack/route.ts
+++
@@ -1,7 +0,0 @@
-export async function POST(req: Request) {
-  console.log("[Paystack Callback]");
-  console.log(await req.json());
-  console.log("[Paystack Callback]");
-
-  return new Response("OK", { status: 200 });
-}
