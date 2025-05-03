e0b160b8c900481df7a6bbe05118c5e245004edd
Mirror83 <glenochieng045@gmail.com>
2025-05-03 11:12:40 +0300

    refactor: replace table with data-table component for units page

--- src/app/(protected)/properties/[id]/units/page.tsx
+++ src/app/(protected)/properties/[id]/units/page.tsx
@@ -1,12 +1,6 @@
+import { unitTableColumns } from "~/components/table-columns/units";
 import { Button } from "~/components/ui/button";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "~/components/ui/table";
+import { DataTable } from "~/components/ui/data-table";
 import { getUnits } from "~/server/actions/units";
 
 type Params = Promise<{ id: string }>;
@@ -15,6 +9,7 @@ export default async function UnitsPage({ params }: { params: Params }) {
   const { id } = await params;
 
   const units = await getUnits(id);
+  console.log("Units", units);
 
   if (units.length === 0) {
     return <Button>Create Unit</Button>;
@@ -23,29 +18,7 @@ export default async function UnitsPage({ params }: { params: Params }) {
   return (
     <div>
       <h1 className="my-4 text-2xl font-bold">Units</h1>
-      <Table className="max-w-[500px]">
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-[100px]">Unit Name</TableHead>
-            <TableHead className="w-[100px]">Unit Type</TableHead>
-            <TableHead className="w-[100px]">Is Occupied</TableHead>
-            <TableHead className="text-right">Rent Price</TableHead>
-          </TableRow>
-        </TableHeader>
-
-        <TableBody>
-          {units.map((unit) => (
-            <TableRow key={unit.id}>
-              <TableCell className="font-medium">{unit.name}</TableCell>
-              <TableCell className="font-medium">{unit.unitType}</TableCell>
-              <TableCell className="font-medium">
-                {unit.occupied ? "Yes" : "No"}
-              </TableCell>
-              <TableCell className="text-right">{unit.rentPrice}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+      <DataTable columns={unitTableColumns} data={units} />
     </div>
   );
 }
